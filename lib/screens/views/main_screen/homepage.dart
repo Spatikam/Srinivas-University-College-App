95d5133cb2ee86f57d33815f6ff65caa15e2472c
A K Kavan <kavankulal2254@gmail.com>
2025-10-26 19:15:30 +0530

    API issue fix and pop-up UI update
    Signed-off-by: A K Kavan <kavankulal2254@gmail.com>

--- lib/screens/views/main_screen/homepage.dart
+++ lib/screens/views/main_screen/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:rip_college_app/screens/popup_screen/image_popup_screen.dart' as image_popup;
+//import 'package:rip_college_app/screens/popup_screen/popup_screen.dart';
 import 'package:rip_college_app/screens/views/base_screen/base_page.dart';
 import 'package:rip_college_app/screens/widget_common/appbar.dart';
 
@@ -95,6 +97,7 @@ class _NavigationScreenState extends State<NavigationScreen> {
   };
 
   final TextEditingController _searchController = TextEditingController();
+  bool _showPopup = true; 
 
   
   /*   IF NOTIFICATION IS REQUIRED FOR POPUP USE THIS
@@ -191,180 +194,202 @@ class _NavigationScreenState extends State<NavigationScreen> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CarouselSlider(
-              options: CarouselOptions(
-                height: 220,
-                autoPlay: true,
-                autoPlayInterval: Duration(seconds: 3),
-                enlargeCenterPage: true,
-                aspectRatio: 16 / 9,
-              ),
-              items: imageUrls.map((url) {
-                int index = imageUrls.indexOf(url);
-                return Builder(
-                  builder: (BuildContext context) {
-                    return ClipRRect(
-                      borderRadius: BorderRadius.circular(15), // Clip the entire container
-                      child: Stack(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: Image.asset(
+
+bool _showImagePopup = true;
+
+void _toggleImagePopup() {
+  setState(() {
+    _showImagePopup = !_showImagePopup;
+  });
+}
+
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: CustomAppBar(),
+    body: Stack(
+      children: [
+        // 🌄 Your existing scrollable content
+        SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 220,
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 3),
+                  enlargeCenterPage: true,
+                  aspectRatio: 16 / 9,
+                ),
+                items: imageUrls.map((url) {
+                  int index = imageUrls.indexOf(url);
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Stack(
+                          children: [
+                            Image.asset(
                               url,
                               fit: BoxFit.cover,
                               width: double.infinity,
                               height: double.infinity,
                             ),
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            left: 0,
-                            right: 0,
-                            child: Container(
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                color: Colors.black.withOpacity(0.5),
-                                borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(15),
-                                  bottomRight: Radius.circular(15),
+                            Positioned(
+                              bottom: 0,
+                              left: 0,
+                              right: 0,
+                              child: Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  color: Colors.black.withOpacity(0.5),
+                                  borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(15),
+                                    bottomRight: Radius.circular(15),
+                                  ),
                                 ),
-                              ),
-                              child: Text(
-                                imageDescriptions[index],
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontFamily: 'Kanit',
+                                child: Text(
+                                  imageDescriptions[index],
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontFamily: 'Kanit',
+                                  ),
+                                  textAlign: TextAlign.center,
                                 ),
-                                textAlign: TextAlign.center,
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+              const SizedBox(height: 20),
+              // 🔍 Search Bar
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(25),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
                       ),
-                    );
-                  },
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(25),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.3),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: TextField(
-                  controller: _searchController,
-                  onChanged: _searchInstitutions,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                    hintText: 'Search institutions...',
-                    border: InputBorder.none,
-                    suffixIcon: IconButton(
-                      icon: Icon(Icons.clear, color: Colors.grey),
-                      onPressed: () {
-                        _searchController.clear(); // Clears the text
-                        setState(() {
-                          filteredInstitutions = institutions;
-                        });
-                      },
+                    ],
+                  ),
+                  child: TextField(
+                    controller: _searchController,
+                    onChanged: _searchInstitutions,
+                    decoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      hintText: 'Search institutions...',
+                      border: InputBorder.none,
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.clear, color: Colors.grey),
+                        onPressed: () {
+                          _searchController.clear();
+                          setState(() {
+                            filteredInstitutions = institutions;
+                          });
+                        },
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: GridView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                  childAspectRatio: 3 / 2.25,
-                ),
-                itemCount: filteredInstitutions.length,
-                itemBuilder: (context, index) {
-                  final institution = filteredInstitutions[index];
+              const SizedBox(height: 20),
 
-                  return GestureDetector(
-                    onTap: () {
-                      // Navigate directly to HomeScreen when "Engineering" is clicked
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => MyHomePage(
-                            collegeName: institution,
+              // 🏫 Institution Grid
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    childAspectRatio: 3 / 2.25,
+                  ),
+                  itemCount: filteredInstitutions.length,
+                  itemBuilder: (context, index) {
+                    final institution = filteredInstitutions[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                MyHomePage(collegeName: institution),
                           ),
-                        ),
-                      );
-                    },
-                    child: AnimatedContainer(
-                      duration: Duration(milliseconds: 300),
-                      decoration: BoxDecoration(
-                        color: institutionColors[index % institutionColors.length],
-                        borderRadius: BorderRadius.circular(15),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.2),
-                            spreadRadius: 2,
-                            blurRadius: 6,
-                            offset: Offset(0, 4),
-                          ),
-                        ],
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            institutionLogos[index % institutionLogos.length],
-                            height: 50,
-                            width: 50,
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            institution,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontFamily: 'Kanit',
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
+                        );
+                      },
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 300),
+                        decoration: BoxDecoration(
+                          color: institutionColors[
+                              index % institutionColors.length],
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.2),
+                              spreadRadius: 2,
+                              blurRadius: 6,
+                              offset: Offset(0, 4),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              institutionLogos[
+                                  index % institutionLogos.length],
+                              height: 50,
+                              width: 50,
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              institution,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontFamily: 'Kanit',
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
+
+        // 🌈 Liquid Glass Popup overlay (only when active)
+        if (_showImagePopup)
+          image_popup.LiquidImagePopup(
+        imagePath: 'assets/images/poster.jpg',
+        websiteUrl: 'https://your-college-website.com',
+        onClose: _toggleImagePopup,
+        //lottiePath: "assets/images/animation/ak.json",
       ),
-    );
-  }
+      ],
+    ),
+  );
+}
+
 }
